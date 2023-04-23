a8177467bd4435acfb7746a932a8358bec150c6f
IT21214134 <it21214134@my.sliit.lk>
2023-04-23 12:48:12 +0530

    Commit Login function and Managing the Dashboard

---
+++ app/src/main/java/com/example/expensestracker/AddNew_Expense_Dashbord.kt
@@ -0,0 +1,11 @@
+package com.example.expensestracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddNew_Expense_Dashbord : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_new_expense_dashbord)
+    }
+}
