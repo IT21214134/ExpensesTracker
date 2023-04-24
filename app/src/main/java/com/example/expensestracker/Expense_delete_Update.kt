309623e280b27bb9cceb7b9c9ce4a71b18a5bb64
RenujaWanigasooriya <127537731+RenujaWanigasooriya@users.noreply.github.com>
2023-04-24 16:58:42 +0530

    Commit Expense layouts

---
+++ app/src/main/java/com/example/expensestracker/Expense_delete_Update.kt
@@ -0,0 +1,11 @@
+package com.example.expensestracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Expense_delete_Update : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_expense_delete_update)
+    }
+}
